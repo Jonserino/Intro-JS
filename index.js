b54bec1cb710c59ee735dd024ebe02e4cde0643c
Jonserino <jonas05@outlook.com>
2022-09-05 18:59:10 +0200

    A lot
    Had to change the folder name for the Number Game.
    Changed the CSS to the main index file.
    Added a link to get to Number Game through the main index file.

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Lager en tekstboks som spør brukeren hva de heter. Vi lagrer navnet i variabelen navn */
+var navn = prompt("Hei, hva heter du?");
+
+// Skriver verdien i variabelen navn til konsollen
+console.log(navn);
+
+// Lager en beskjedboks som skriver en hilsen til brukeren
+alert("Hei på deg " + navn + "!");
