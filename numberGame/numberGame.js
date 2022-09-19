636d14b40c944c24bcfabfb661ea9701d75f9a60
Jonserino <jonas05@outlook.com>
2022-09-19 10:03:16 +0200

    Timer fixed
    I fixed the timer, working a little on the attempts again.

--- numberGame/numberGame.js
+++ numberGame/numberGame.js
@@ -2,6 +2,7 @@ function getRandomInt(max) {
     return  Math.floor(Math.random() * max + 1);
 }
 
+playing = true;
 
 var correctNumber = getRandomInt(100);
 console.log("Correct number: " + correctNumber);
@@ -13,7 +14,24 @@ var input = document.querySelector("#inputID");
 var reset = document.querySelector("#restartID");
 var max_time = 30
 var time_left = max_time;
-var timer_EL = document.querySelector("#timerID");
+var timer_EL = document.querySelector("#timer");
+
+var timer = setInterval(myTimer, 1000); // kjører funksjonen myTimer 1 gang i sekundet
+
+function myTimer(){
+    if (playing == true) {
+        if (time_left > 0){
+            time_left--; // reduserer time_left var med 1
+            timer_EL.innerHTML = "Time left: " + time_left; // setter tallet i time_left inn i HTML tag, timer_EL
+            playing = true;
+        } else {
+            console.log("Player ran out of time.")
+            console.log("Player lost.")
+            document.getElementById("output").innerHTML="You ran out of time. :< Please try again! The number was: " + correctNumber;
+            playing = false;
+        }
+    }
+}
 
 function checkAttempts(){
     if (guesses <= 0) {
@@ -21,20 +39,22 @@ function checkAttempts(){
         console.log("Guesses left: " + guesses);
         console.log("Player lost")
         playing = false;
-        if (time_left >= 0) {
-
-            document.getElementById("output").innerHTML="You rna out of time! Please restart, the number was: " + correctNumber;
-            console.log("Player ran out of time");
-            console.log("Player lost");
-        }
     } else {
-        console.log("Guesses left: " + guesses);
-        playing = true;
+        playing = true;  
+    }
+}
+
+function guessing(x) {
+    if (guesses == 1) {
+        document.getElementById("output").innerHTML= x + guesses + " Guess left.";  
+    } else {
+        document.getElementById("output").innerHTML= x + guesses + " Guesses left.";
     }
 }
 
 function restart() {
     correctNumber = getRandomInt(100);
+    console.log(correctNumber)
     guesses = 10;
     document.getElementById("output").innerHTML="";
     input.value = "";
@@ -43,24 +63,31 @@ function restart() {
 
 function checkNumber(){
     if (playing == true) {
+        console.log("Guesses left: " + guesses);
         console.log("Player input: " + input.value)
         console.log("Correct number: " + correctNumber)
         if (input.value == correctNumber) {
-            document.getElementById("output").innerHTML="Congratulations! You were able to guess the correct number with " + guesses + " attempt(s) remaining! Restart and try again :>";
-            console.log("Player won")
-            playing = false;
+            if (guesses == 1) {
+                guesses + 2;
+                document.getElementById("output").innerHTML="Congratulations! You were able to guess the correct number with " + guesses + " attempt remaining! Restart and try again :>";
+                console.log("Player won")
+                playing = false;    
+            } else {
+                document.getElementById("output").innerHTML="Congratulations! You were able to guess the correct number with " + guesses + " attempts remaining! Restart and try again :>";
+                console.log("Player won")
+                playing = false;    
+            }
         } else {
             if (input.value > correctNumber) {
-                guesses--;
-                document.getElementById("output").innerHTML="Too high! " + guesses + " Guesses left.";
+                guessing("Too high ");            
                 checkAttempts();
             } else {
-                guesses--;
-                document.getElementById("output").innerHTML="Too low! " + guesses + " Guesses left.";
-                checkAttempts();
+                guessing("Too low ");
+                checkAttempts(); 
             }
         }
-    }
+        guesses--;
+    } 
 }
 
 timer_EL.innerHTML = "Time left: " + time_left;
@@ -77,11 +104,3 @@ window.addEventListener('keyup', function(event) {
     }
 });
 
-var timer = setInterval(myTimer, 1000); // kjører funksjonen myTimer 1 gang i sekundet
-
-function myTimer(){
-    if (playing = true){
-        time_left--; // reduserer time_left var med 1
-        timer_EL.innerHTML = "Time left: " + time_left; // setter tallet i time_left inn i HTML tag, timer_EL
-    }
-}
